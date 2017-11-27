483a80c4229de5e9b76cf1eec1a753078bc84f4d
Matthew Curtis Price <mcp97@awing.cs.byu.edu>
2017-11-27 10:24:24 -0700

    moved files so it works in a jar

--- src/Model/GameMap.java
+++ src/Model/GameMap.java
@@ -3,6 +3,9 @@ package Model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -47,32 +50,50 @@ public class GameMap {
 
 
     private void createCities() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("citynames.txt"));
-        while(scanner.hasNextLine())
-        {
-            String city=scanner.nextLine();
-            cityList.add(new City(city));
-        }
+        URL url = this.getClass().getResource("/" + "citynames.txt");
+        InputStream stream = null;
+		try {
+			stream = url.openStream();
+	        Scanner scanner = new Scanner(stream);
+	        while(scanner.hasNextLine())
+	        {
+	            String city=scanner.nextLine();
+	            cityList.add(new City(city));
+	        }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     private void createRoutes() throws FileNotFoundException
     {
-        Scanner scanner = new Scanner(new File("routes.txt"));
-        while(scanner.hasNextLine())
-        {
-            String cityname1 = scanner.next();
-            String cityname2 = scanner.next();
-            String color = scanner.next();
-            String length = scanner.next();
+    	URL url = this.getClass().getResource("/" + "routes.txt");
+        InputStream stream = null;
 
-            City city1 = getCity(cityname1);
-            City city2 = getCity(cityname2);
-            Route newRoute = new Route(city1, city2, color, length);
-            routeList.add(newRoute);
-            //city1.addConnectedRoute(newRoute);
-            //city2.addConnectedRoute(newRoute);
-        }
-        System.out.println("finished routes");
+		try {
+			stream = url.openStream();
+			Scanner scanner = new Scanner(stream);
+	        while(scanner.hasNextLine())
+	        {
+	            String cityname1 = scanner.next();
+	            String cityname2 = scanner.next();
+	            String color = scanner.next();
+	            String length = scanner.next();
+
+	            City city1 = getCity(cityname1);
+	            City city2 = getCity(cityname2);
+	            Route newRoute = new Route(city1, city2, color, length);
+	            routeList.add(newRoute);
+	            //city1.addConnectedRoute(newRoute);
+	            //city2.addConnectedRoute(newRoute);
+	        }
+	        System.out.println("finished routes");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 
     private City getCity(String name)
