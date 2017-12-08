9f35f3afc815a21a4acf64b9db71a917080f6092
Antman537 <Antman537@yahoo.com>
2017-12-08 10:50:23 -0700

    createFactory
    Updated main to check for arguments and set the DAOs

--- src/Server/ServerCommunicator.java
+++ src/Server/ServerCommunicator.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
+import Common.IDAOFactory;
 import Common.IGameDAO;
 import Common.IUserDAO;
+import Model.ServerModel;
 //TODO: add functionality when set outside program
 import plugin.*;
 public class ServerCommunicator {
@@ -21,9 +23,6 @@ public class ServerCommunicator {
 	public static final String toLowerCase_DESIGNATOR = "/toLowerCase";
 	public static final String trim_DESIGNATOR = "/trim";
 	public static final String parseInteger_DESIGNATOR = "/parseInteger";
-	
-	private IGameDAO gameDAO;
-	private IUserDAO userDAO;
 	
 	
 	private HttpServer server;
@@ -61,10 +60,45 @@ public class ServerCommunicator {
 	private void setupContext() {
 		server.createContext(DEFAULT_DESIGNATOR, new Handler());
 	}
+	
+	/*
+	 * uses input dbType to create factory of given type
+	 * @pre: dbType must be the name of the factory class
+	 * @post: loads the databases to the serverModel
+	 */
+	private static void createFactory(String dbType) {
+		Class<?> c = null;
+		IDAOFactory factory = null;
+		String url = "plugin."+dbType;
+		try {
+			c = Class.forName(url);
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		try {
+			factory = (IDAOFactory) c.newInstance();
+		}
+		catch (InstantiationException e) {
+			e.printStackTrace();
+			return;
+		}
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+			return;
+		}
+		ServerFacade.getSingleton().setUserDAO(factory.createUserDAO());
+		ServerFacade.getSingleton().setGameDAO(factory.createGameDAO());
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if(args.length > 0) SERVER_PORT_NUMBER = Integer.parseInt(args[0]);
+		if(args.length > 0) {
+			createFactory(args[0]);
+			if (args.length>1) {
+				ServerFacade.getSingleton().setMaxNumCommands(Integer.parseInt(args[1]));
+			}
+		}
 		new ServerCommunicator().run();
 	}
 
