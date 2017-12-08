5887158488450c9a734f203712dbdeec1b3bac62
AbramBeazer <32072243+AbramBeazer@users.noreply.github.com>
2017-12-08 10:39:12 -0700

    DeleteGame and LoadGame
    DAO can now delete game from the database and load all games from the database.

--- src/plugin/SQL_GameDAO.java
+++ src/plugin/SQL_GameDAO.java
@@ -7,8 +7,10 @@ import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import Model.AuthToken;
@@ -64,12 +66,49 @@ public class SQL_GameDAO implements IGameDAO {
 
 	public List<Game> loadGames()
 	{
-		return null;
+		List<Game> games = new ArrayList<Game>();
+		Connection c = null;
+		Statement statement = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.sqlite");
+            statement = c.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT game FROM Games;");
+            if(rs.isClosed() == true)
+            {
+                return games;
+            }
+            while(rs.next())
+            {
+               games.add((Game) rs.getBlob("game"));
+            }
+            statement.close();
+            c.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException se){
+            se.printStackTrace();
+        }		
+		return games;
 	}
 
 	public void deleteGame(String gameID)
 	{
-		
+		Connection c = null;
+        PreparedStatement statement = null;
+        String s = "DELETE FROM Games WHERE gameID = " + gameID + ";";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.sqlite");
+            statement = c.prepareStatement(s);
+            statement.executeUpdate();
+            statement.close();
+            c.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException se){
+            se.printStackTrace();
+        }
 	}
 
 	public List<ICommand> loadCommands(String gameID)
